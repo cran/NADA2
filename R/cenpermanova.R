3b7fd4c5dfa475ee55c9ff0889f9bcc6d6891fb6
Paul Julian <pauljulianphd@gmail.com>
2021-04-30 09:10:10 +0000

    version 1.0.1

--- R/cenpermanova.R
+++ R/cenpermanova.R
@@ -21,7 +21,6 @@
 #'
 #' data(PbHeron)
 #' cenpermanova(PbHeron$Liver,PbHeron$LiverCen,PbHeron$DosageGroup)
-
 cenpermanova <- function(y1, y2, grp, R = 9999,printstat=TRUE) {
   yname <- deparse(substitute(y1))
   gname <- deparse(substitute(grp))
@@ -42,9 +41,8 @@ cenpermanova <- function(y1, y2, grp, R = 9999,printstat=TRUE) {
   # see "Permutation Tests, Second Edition" by Good (2000), page 44.
   Wprime.groups <- function (x, Factor) {
     Wprime <- 0
-    gpname <- as.character(levels(Factor))
     for (i in 1:nlevels(Factor)) {
-      W.grp <- (sum(x[Factor == gpname[i]]))^2 / length(x[Factor == gpname[i]])
+      W.grp <- (sum(x[Factor == grpname[i]]))^2 / length(x[Factor == grpname[i]])
       Wprime <- Wprime + W.grp
     }
     return(invisible(Wprime))
@@ -68,8 +66,8 @@ cenpermanova <- function(y1, y2, grp, R = 9999,printstat=TRUE) {
 
   # group means
   for (i in 1:nlevels(Factor)) {
-    ros.out <- suppressWarnings(cenros(x1[Factor == gpname[i]], as.logical(x2[Factor == gpname[i]])))
-    group.means[i] <- signif(mean(ros.out),4)
+    ros.out <- suppressWarnings(cenros(x1[Factor == grpname[i]], as.logical(x2[Factor == grpname[i]])))
+    group.means[i] <- signif(mean(ros.out$modeled),4)
     mean.names[i] <- paste("mean(", gpname[i], ")", sep="")
   }
   names(group.means) <- mean.names
@@ -79,9 +77,9 @@ cenpermanova <- function(y1, y2, grp, R = 9999,printstat=TRUE) {
   p.hi <- max(pval.lo, pval.hi)
   result <- data.frame(teststat.lo, teststat.hi, p.lo, p.hi, group.means)
   if(printstat==TRUE){
-  cat(" Permutation test of mean CensData:", yname, "  by Factor:", gname, '\n', "   ", R, "Permutations", "\n")
-  cat( "Test Statistic =", signif(teststat.lo, 4), "to", signif(teststat.hi, 4), "      p =", pval.lo, "to", pval.hi, '\n', "\n")
-  print(group.means, row.names = FALSE, print.gap = 3)
+    cat(" Permutation test of mean CensData:", yname, "  by Factor:", gname, '\n', "   ", R, "Permutations", "\n")
+    cat( "Test Statistic =", signif(teststat.lo, 4), "to", signif(teststat.hi, 4), "      p =", pval.lo, "to", pval.hi, '\n', "\n")
+    print(group.means, row.names = FALSE, print.gap = 3)
   }
   return(invisible(result))
 }
